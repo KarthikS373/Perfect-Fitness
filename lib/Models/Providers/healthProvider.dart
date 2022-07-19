95a291ad54a81d3ebc330cce23ddc0ee14588f7d
spartan <lit2021011@iiitl.ac.in>
2022-07-20 00:19:07 +0530

    Providers Added

--- lib/Models/Providers/healthProvider.dart
+++ lib/Models/Providers/healthProvider.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 class HealthProvider with ChangeNotifier{
   // Variables
   int _steps = 0;
+  var d = [0.0, 1.0, 2.0, -1.5];
 
   //Functions
 
@@ -14,6 +15,12 @@ class HealthProvider with ChangeNotifier{
 
   void incrementSteps() {
     _steps++;
+    notifyListeners();
+  }
+
+  void heartBeatListener(var bpm , d)
+  {
+    d.add((bpm - 70).toDouble());
     notifyListeners();
   }
 
--- lib/Screens/CalorieScreen/calorieScreen.dart
+++ lib/Screens/CalorieScreen/calorieScreen.dart
@@ -1,7 +1,10 @@
 import 'package:fitness_monitoring/Utils/Theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
+import '../../Models/Providers/healthProvider.dart';
 
 class CalorieScreen extends StatefulWidget {
   const CalorieScreen({Key? key}) : super(key: key);
@@ -19,98 +22,102 @@ class _CalorieScreenState extends State<CalorieScreen> {
     final media = MediaQuery.of(context);
     final theme = Theme.of(context);
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: media.size.height * 0.4,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: secondaryColor,
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(50),
-                bottomLeft: Radius.circular(50),
-              ),
-            ),
-            child: SleekCircularSlider(
-              initialValue: (_calorie * 0.045),
-              appearance: CircularSliderAppearance(
-                customColors: CustomSliderColors(
-                  dotColor: textColor,
-                  dynamicGradient: true,
-                  progressBarColor: waterColor,
-                  trackColor: Colors.grey,
+      body: Consumer<HealthProvider>(
+        builder: (context, value, child) {
+          return Column(
+            children: [
+              Container(
+                height: media.size.height * 0.4,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: secondaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(50),
+                    bottomLeft: Radius.circular(50),
+                  ),
                 ),
-                size: media.size.height * 0.5,
-                animationEnabled: true,
-                customWidths: CustomSliderWidths(
-                  progressBarWidth: 8,
-                  handlerSize: 4,
-                  shadowWidth: 10,
-                  trackWidth: 1,
-                ),
-              ),
-              min: 0,
-              max: 10000,
-              innerWidget: (double val) {
-                return Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Text(
-                        "${(_calorie * 0.045).toStringAsPrecision(2)} cal",
-                        style: GoogleFonts.nunito(
-                          color: textColor,
-                          fontSize: 30.0,
+                child: SleekCircularSlider(
+                  initialValue: (value.getSteps * 0.045),
+                  appearance: CircularSliderAppearance(
+                    customColors: CustomSliderColors(
+                      dotColor: textColor,
+                      dynamicGradient: true,
+                      progressBarColor: waterColor,
+                      trackColor: Colors.grey,
+                    ),
+                    size: media.size.height * 0.5,
+                    animationEnabled: true,
+                    customWidths: CustomSliderWidths(
+                      progressBarWidth: 8,
+                      handlerSize: 4,
+                      shadowWidth: 10,
+                      trackWidth: 1,
+                    ),
+                  ),
+                  min: 0,
+                  max: 100,
+                  innerWidget: (double val) {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Text(
+                            "${(value.getSteps * 0.045).toStringAsPrecision(2)} cal",
+                            style: GoogleFonts.nunito(
+                              color: textColor,
+                              fontSize: 30.0,
+                            ),
+                          ),
                         ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              SizedBox(
+                width: media.size.width * 0.8,
+                child: TextField(
+                  onEditingComplete: () {
+                    setState(
+                          () {
+                        _calorie = value.getSteps;
+                      },
+                    );
+                    _textController.clear();
+                  },
+                  controller: _textController,
+                  cursorColor: textColor.withOpacity(0.5),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        color: textColor.withOpacity(0.5),
+                        width: 1,
                       ),
                     ),
-                  ],
-                );
-              },
-            ),
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          SizedBox(
-            width: media.size.width * 0.8,
-            child: TextField(
-              onEditingComplete: () {
-                setState(
-                  () {
-                    _calorie = int.parse(_textController.text);
-                  },
-                );
-                _textController.clear();
-              },
-              controller: _textController,
-              cursorColor: textColor.withOpacity(0.5),
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(
-                    color: textColor.withOpacity(0.5),
-                    width: 1,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: secondaryColor,
+                        width: 1,
+                      ),
+                    ),
+                    hintText: "Number of steps",
+                    hintStyle: GoogleFonts.nunito(
+                      color: secondaryColor,
+                    ),
                   ),
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: const BorderSide(
-                    color: secondaryColor,
-                    width: 1,
-                  ),
-                ),
-                hintText: "Enter Number of steps",
-                hintStyle: GoogleFonts.nunito(
-                  color: secondaryColor,
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
+              )
+            ],
+          );
+        }
+    )
     );
   }
 }
