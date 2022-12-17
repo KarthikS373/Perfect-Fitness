04ce02cd5258999eb4eb8acd132d29ebd83781aa
ArpitaJJ <97668169+ArpitaJJ@users.noreply.github.com>
2022-12-18 02:12:31 +0530

    Implemented smooth rendering and transitions

--- lib/Screens/HomeScreen/HomeScreen.dart
+++ lib/Screens/HomeScreen/HomeScreen.dart
@@ -1,1 +1,42 @@
+import 'package:fitness_monitoring/Models/excersiseDB.dart';
+import 'package:fitness_monitoring/Screens/HomeScreen/homeGrids.dart';
+import 'package:fitness_monitoring/Screens/HomeScreen/homeHeader.dart';
+import 'package:fitness_monitoring/Screens/HomeScreen/homeRecentActivities.dart';
+import 'package:fitness_monitoring/Screens/HomeScreen/homeStepCount.dart';
+import 'package:fitness_monitoring/Widgets/Cards/fitnessCard.dart';
+import 'package:fitness_monitoring/Widgets/Drawer/drawer.dart';
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const HomeDrawer(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: const [
+              HomeHeader(),
+              HomeStepCount(),
+              HomeGrids(),
+              FitnessCard(
+                title: "Exercise",
+                subtitle: "The perfect fitness",
+                icons: [
+                  Icons.add,
+                  Icons.add,
+                  Icons.add,
+                  Icons.add,
+                ],
+              ),
+              HomeRecentActivities(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
